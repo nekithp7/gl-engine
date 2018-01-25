e1d8e7297f9de5536a98eea18760d981e0992d69
Nikita Mynych <nekithp7@gmail.com>
2018-01-25 20:01:09 +0200

    strafe added

--- engine/Entities/Player.cs
+++ engine/Entities/Player.cs
@@ -10,7 +10,7 @@ namespace engine.Entities
 {
 	public class Player : Entity
 	{
-		private const float MOVE_SPEED = 0.1f;		
+		private const float MOVE_SPEED = 0.1f;
 
 		private Camera camera;
 		private float currentSpeed = 0.0f;
@@ -18,18 +18,31 @@ namespace engine.Entities
 		public Player(TexturedModel model, Vector3 position, Vector3 rotation, float scale)
 			: base(model, position, rotation, scale) => camera = new Camera(position);
 
-		public Camera Camera { get => camera; set => camera = value; }
+		public Camera Camera => camera;
 
 		public void Move(Key key)
 		{
+			bool isStrafe = false;
 			switch (key)
 			{
 				case Key.W:
 					currentSpeed = -MOVE_SPEED;
+					isStrafe = false;
 					break;
 
 				case Key.S:
 					currentSpeed = MOVE_SPEED;
+					isStrafe = false;
+					break;
+
+				case Key.D:
+					currentSpeed = MOVE_SPEED;
+					isStrafe = true;
+					break;
+
+				case Key.A:
+					currentSpeed = -MOVE_SPEED;
+					isStrafe = true;
 					break;
 
 				default:
@@ -37,15 +50,22 @@ namespace engine.Entities
 					break;
 			}
 
-			float xDelta = currentSpeed * (float)Sin(ToRad(rotation.Y));
-			float zDelta = currentSpeed * (float)Cos(ToRad(rotation.Y));
+			var (offsetX, offsetZ) = GetPosition(isStrafe);
 
-			Translate(new Vector3(xDelta, 0.0f, zDelta));
+			Translate(new Vector3(offsetX, 0.0f, offsetZ));
 		}
 
 		public void Rotate(int xDelta, int yDelta)
 		{
 			Rotate(new Vector3(0, -xDelta * 0.1f, 0));
+		}
+
+		private (float offsetX, float offsetZ) GetPosition(bool isStrafe)
+		{
+			float offsetX = currentSpeed * (float)Sin(ToRad(rotation.Y));
+			float offsetZ = currentSpeed * (float)Cos(ToRad(rotation.Y));
+
+			return isStrafe ? (offsetZ, offsetX) : (offsetX, offsetZ);
 		}
 	}
 }
